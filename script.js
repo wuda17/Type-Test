5a1e33f8990f7315ed423081c53868cb965e3a7b
wuda17 <84358223+wuda17@users.noreply.github.com>
2022-03-24 21:29:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const RANDOME_QUOTE_API_URL = 'http://api.quotable.io/random'
 const quoteDisplayElement = document.getElementById('quoteDisplay')
-const quoteInputElement = document.getElementById('quoteInput')
+const quoteInputElement = document.getElementById('quote-input')
 const timerElement = document.getElementById('timer')
 
 //
